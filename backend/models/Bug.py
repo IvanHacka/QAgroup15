e2b32b3fb5694c73a57e9d84bf1a520eb54ba026
Vincci To <veildths.@VinccideMacBook-Air.local>
2025-12-30 12:58:34 +0800

    Complete Sprint3 user story30 and fixed bugs

--- backend/models/Bug.py
+++ backend/models/Bug.py
@@ -27,6 +27,9 @@ class Bug:
     id: str = field(default_factory=lambda: str(uuid.uuid4()))
     status: BugStatus = BugStatus.OPEN
     priority: BugPriority = BugPriority.LOW
+
+    reopen_count: int = 0  #reopen #30
+
     tester_id: Optional[str] = None
     assigned_to: Optional[str] = None
 
@@ -42,12 +45,14 @@ class Bug:
             "description": self.description,
             "status": self.status.value,
             "priority": self.priority.value,
+            "reopen_count": self.reopen_count,   #reopen #30
             "tester_id": self.tester_id,
             "assigned_to": self.assigned_to,
             "created_at": self.created_at,
             "updated_at": self.updated_at,
             "screenshot": self.screenshot,
         }
+
 
     @staticmethod
     def from_dict(data: dict) -> "Bug":
@@ -68,14 +73,16 @@ class Bug:
             priority = BugPriority.LOW
 
         return Bug(
-            id=data.get("id", str(uuid.uuid4())),
-            title=data["title"],
-            description=data["description"],
-            status=status,
-            priority=priority,
-            tester_id=data.get("tester_id"),
-            assigned_to=data.get("assigned_to"),
-            created_at=data.get("created_at", datetime.now().isoformat()),
-            updated_at=data.get("updated_at"),
-            screenshot=data.get("screenshot", []),
-        )
+    id=data.get("id", str(uuid.uuid4())),
+    title=data["title"],
+    description=data["description"],
+    status=status,
+    priority=priority,
+    reopen_count=data.get("reopen_count", 0),  # reopen #30 !!
+    tester_id=data.get("tester_id"),
+    assigned_to=data.get("assigned_to"),
+    created_at=data.get("created_at", datetime.now().isoformat()),
+    updated_at=data.get("updated_at"),
+    screenshot=data.get("screenshot", []),
+)
+
